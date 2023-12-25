80ca2afde8309dd85258f7e036a343c76ba7cb34
@ipz212_syav <ipz212_syav@student.ztu.edu.ua>
2023-12-25 15:08:06 +0200

    react-hook-form + validation

--- src/ApplicationForm.js
+++ src/ApplicationForm.js
@@ -1,79 +1,121 @@
 // ApplicationForm.js
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppContext } from './useAppContext';
+import { useForm, Controller } from 'react-hook-form';
 
 const ApplicationForm = () => {
     const { addApplication } = useAppContext();
-    const [applicantName, setApplicantName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [patronymic, setPatronymic] = useState('');
-    const [faculty, setFaculty] = useState('');
-    const [group, setGroup] = useState('');
-    const [applicationType, setApplicationType] = useState('scholarship');
     const navigate = useNavigate();
+    const { control, handleSubmit, formState: { errors } } = useForm();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const handleFormSubmit = (data) => {
+        addApplication(data);
 
-        const newApplication = {
-            applicantName,
-            lastName,
-            patronymic,
-            faculty,
-            group,
-            applicationType,
-        };
-
-        addApplication(newApplication);
-
-        setApplicantName('');
-        setLastName('');
-        setPatronymic('');
-        setFaculty('');
-        setGroup('');
-        setApplicationType('scholarship');
-
-        // Після збереження заявки перенаправляємо на сторінку "Заявок"
         navigate('/applications');
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Ім'я:
-                <input type="text" value={applicantName} onChange={(e) => setApplicantName(e.target.value)} required />
-            </label>
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <div>
+                <label>
+                    Ім'я:
+                    <Controller
+                        name="applicantName"
+                        control={control}
+                        defaultValue=""
+                        rules={{ required: 'Ім\'я обов\'язкове поле' }}
+                        render={({ field }) => (
+                            <input type="text" {...field} />
+                        )}
+                    />
+                    {errors.applicantName && <span>{errors.applicantName.message}</span>}
+                </label>
+            </div>
+            <div>
+                <label>
+                    Прізвище:
+                    <Controller
+                        name="lastName"
+                        control={control}
+                        defaultValue=""
+                        rules={{ required: 'Прізвище обов\'язкове поле' }}
+                        render={({ field }) => (
+                            <input type="text" {...field} />
+                        )}
+                    />
+                    {errors.lastName && <span>{errors.lastName.message}</span>}
+                </label>
+            </div>
+            <div>
+                <label>
+                    По-батькові:
+                    <Controller
+                        name="patronymic"
+                        control={control}
+                        defaultValue=""
+                        rules={{ required: 'По-батькові обов\'язкове поле' }}
+                        render={({ field }) => (
+                            <input type="text" {...field} />
+                        )}
+                    />
+                    {errors.patronymic && <span>{errors.patronymic.message}</span>}
+                </label>
+            </div>
+            <div>
+                <label>
+                    Факультет:
+                    <Controller
+                        name="faculty"
+                        control={control}
+                        defaultValue=""
+                        rules={{ required: 'Факультет обов\'язкове поле' }}
+                        render={({ field }) => (
+                            <input type="text" {...field} />
+                        )}
+                    />
+                    {errors.faculty && <span>{errors.faculty.message}</span>}
+                </label>
+            </div>
+            <div>
+                <label>
+                    Група:
+                    <Controller
+                        name="group"
+                        control={control}
+                        defaultValue=""
+                        rules={{ required: 'Група обов\'язкове поле' }}
+                        render={({ field }) => (
+                            <input type="text" {...field} />
+                        )}
+                    />
+                    {errors.group && <span>{errors.group.message}</span>}
+                </label>
+            </div>
 
-            <label>
-                Прізвище:
-                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
-            </label>
+            <div>
+                <label>
+                    Ціль подання заявки:
+                    <Controller
+                        name="applicationType"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => (
+                            <select {...field}>
+                                <option value="scholarship">Студентська стипендія</option>
+                                <option value="financialAid">Фінансова допомога</option>
+                            </select>
+                        )}
+                    />
+                    {errors.applicationType && <span>{errors.applicationType.message}</span>}
+                </label>
+            </div>
 
-            <label>
-                По-батькові:
-                <input type="text" value={patronymic} onChange={(e) => setPatronymic(e.target.value)} required />
-            </label>
-
-            <label>
-                Факультет:
-                <input type="text" value={faculty} onChange={(e) => setFaculty(e.target.value)} required />
-            </label>
-
-            <label>
-                Група:
-                <input type="text" value={group} onChange={(e) => setGroup(e.target.value)} required />
-            </label>
-
-            <label>
-                Ціль подання заявки:
-                <select value={applicationType} onChange={(e) => setApplicationType(e.target.value)}>
-                    <option value="scholarship">Студентська стипендія</option>
-                    <option value="financialAid">Фінансова допомога</option>
-                </select>
-            </label>
-
-            <button type="submit">Відправити</button>
+            <div>
+                <button type="submit">
+                    Відправити
+                </button>
+            </div>
         </form>
     );
 };
